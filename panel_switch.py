ae1948657698aabf88c167d6f233b77af8ae49cb
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2019-10-23 02:53:20 -0700

    first commit

---
+++ panel_switch.py
@@ -0,0 +1,27 @@
+import sublime
+import sublime_plugin
+
+
+class PanelSwitchCommand(sublime_plugin.WindowCommand):
+  def __init__(self, window):
+        super().__init__(window)
+        self.previous_active_group = None
+        self.is_panel_focused = False
+
+  def run(self):
+    terminus = self.window.find_output_panel("Terminus")
+
+    if terminus != None and self.is_panel_focused == True:
+      # terminus is already focused.  switch back to editor
+      if self.previous_active_group != None:
+        self.window.focus_group(self.previous_active_group)
+        self.previous_active_group = None
+        self.is_panel_focused = False
+    else:
+      # store active view
+      self.previous_active_group = self.window.active_group()
+
+      # focus terminus
+      self.window.run_command('show_panel', args={'panel': 'output.Terminus'})
+      self.window.focus_view(self.window.find_output_panel("Terminus"))
+      self.is_panel_focused = True
